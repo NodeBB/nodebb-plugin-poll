2be93f495afd7772b8441e53780d3bc547ab8f7f
Schamper <e_schamper@hotmail.com>
2016-03-12 17:11:26 +0100

    Load poll on edit

--- public/js/poll/main.js
+++ public/js/poll/main.js
@@ -6,8 +6,20 @@ var Poll = {};
 
 	$(window).on('action:topic.loading', function() {
 		if (ajaxify.data.posts.length > 0 && ajaxify.data.posts[0].hasOwnProperty('pollId')) {
-			var pollId = ajaxify.data.posts[0].pollId;
+			getPoll(ajaxify.data.posts[0].pollId);
+		}
+	});
 
+	$(window).on('action:posts.edited', function(ev, data) {
+		if (data.post.hasOwnProperty('pollId')) {
+			getPoll(data.post.pollId);
+		}
+	});
+
+	function getPoll(pollId) {
+		pollId = parseInt(pollId, 10);
+
+		if (!isNaN(pollId)) {
 			Poll.sockets.getPoll({pollId: pollId}, function(err, pollData) {
 				if (err && app.user.uid) {
 					return app.alertError(err.message);
@@ -16,6 +28,6 @@ var Poll = {};
 				Poll.view.load(pollData);
 			});
 		}
-	});
+	}
 
 })();
